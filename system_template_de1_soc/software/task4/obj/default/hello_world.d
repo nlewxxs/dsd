8c2e2f22cfaf8ec0c65c584d6bb4fa183c55d596
Nik <nik.lewis21@imperial.ac.uk>
2024-02-02 16:33:58 +0000

    added start of task4, results img included

---
+++ system_template_de1_soc/software/task4/obj/default/hello_world.d
@@ -0,0 +1,20 @@
+obj/default/hello_world.o: hello_world.c \
+ ../task4_bsp//HAL/inc/sys/alt_stdio.h \
+ ../task4_bsp//HAL/inc/sys/alt_alarm.h \
+ ../task4_bsp//HAL/inc/sys/alt_llist.h ../task4_bsp//HAL/inc/alt_types.h \
+ ../task4_bsp//HAL/inc/priv/alt_alarm.h ../task4_bsp/system.h \
+ ../task4_bsp/linker.h
+
+../task4_bsp//HAL/inc/sys/alt_stdio.h:
+
+../task4_bsp//HAL/inc/sys/alt_alarm.h:
+
+../task4_bsp//HAL/inc/sys/alt_llist.h:
+
+../task4_bsp//HAL/inc/alt_types.h:
+
+../task4_bsp//HAL/inc/priv/alt_alarm.h:
+
+../task4_bsp/system.h:
+
+../task4_bsp/linker.h:
