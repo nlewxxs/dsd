410f6604dea7ac143c82f63a8760cf35347fa26b
Nik <nik.lewis21@imperial.ac.uk>
2024-03-03 00:54:05 +0000

    Cleanup testbench

--- playground/cpp/tb.cpp
+++ playground/cpp/tb.cpp
@@ -1,10 +1,22 @@
 /* A testbench to verify a DUT using Verilator */
 
+#include <array>
 #include <iostream>
 #include <bitset>
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 #include "Vtop.h"
+
+namespace {
+static constexpr std::array<unsigned int, 6> kTestCases {
+    0b00111110000000000000000000000000, // 0.125
+    0b00111101100000000000000000000000, // 0.0625
+    0b10111101100000000000000000000000, // -0.0625
+    0b00000000000000000000000000000000, // 0
+    0b00111111100000000000000000000000, // 1
+    0b10111111100000000000000000000000 // -1
+};
+}
 
 int main(int argc, char** argv, char** env) {
 
@@ -18,70 +30,17 @@ int main(int argc, char** argv, char** env) {
     dut->trace(tfp, 99);
     tfp->open("top.vcd");
 
-    // initialise
-    dut->clk = 0;
-    dut->in = 0b00111110000000000000000000000000; // 0.125
-    dut->eval();
-    dut->clk = !dut->clk;
-    dut->eval();
-    std::cout << "Converting: \t" << std::hex << dut->in << " \t| " << std::bitset<32>(dut->in) << std::endl;
-    std::cout << "int: \t\t" << std::hex << dut->int_out << " \t| " << std::bitset<2>(dut->int_out) << std::endl;
-    std::cout << "frac: \t\t" << std::hex << dut->frac_out << " \t| " << std::bitset<23>(dut->frac_out) << std::endl;
-
-    std::cout << "-----------------------------" << std::endl;
-
-    dut->clk = 0;
-    dut->in = 0b00111101100000000000000000000000; // 0.0625
-    dut->eval();
-    dut->clk = !dut->clk;
-    dut->eval();
-    std::cout << "Converting: \t" << std::hex << dut->in << " \t| " << std::bitset<32>(dut->in) << std::endl;
-    std::cout << "int: \t\t" << std::hex << dut->int_out << " \t| " << std::bitset<2>(dut->int_out) << std::endl;
-    std::cout << "frac: \t\t" << std::hex << dut->frac_out << " \t| " << std::bitset<23>(dut->frac_out) << std::endl;
-
-    std::cout << "-----------------------------" << std::endl;
-
-    dut->clk = 0;
-    dut->in = 0b10111101100000000000000000000000; // -0.0625
-    dut->eval();
-    dut->clk = !dut->clk;
-    dut->eval();
-    std::cout << "Converting: \t" << std::hex << dut->in << " \t| " << std::bitset<32>(dut->in) << std::endl;
-    std::cout << "int: \t\t" << std::hex << dut->int_out << " \t| " << std::bitset<2>(dut->int_out) << std::endl;
-    std::cout << "frac: \t\t" << std::hex << dut->frac_out << " \t| " << std::bitset<23>(dut->frac_out) << std::endl;
-
-    std::cout << "-----------------------------" << std::endl;
-
-    dut->clk = 0;
-    dut->in = 0b10110110101110110001100101111010; // -0.0000005576
-    dut->eval();
-    dut->clk = !dut->clk;
-    dut->eval();
-    std::cout << "Converting: \t" << std::hex << dut->in << " \t| " << std::bitset<32>(dut->in) << std::endl;
-    std::cout << "int: \t\t" << std::hex << dut->int_out << " \t| " << std::bitset<2>(dut->int_out) << std::endl;
-    std::cout << "frac: \t\t" << std::hex << dut->frac_out << " \t| " << std::bitset<23>(dut->frac_out) << std::endl;
-
-    std::cout << "-----------------------------" << std::endl;
-
-    dut->clk = 0;
-    dut->in = 0b00000000000000000000000000000000; // 0
-    dut->eval();
-    dut->clk = !dut->clk;
-    dut->eval();
-    std::cout << "Converting: \t" << std::hex << dut->in << " \t| " << std::bitset<32>(dut->in) << std::endl;
-    std::cout << "int: \t\t" << std::hex << dut->int_out << " \t| " << std::bitset<2>(dut->int_out) << std::endl;
-    std::cout << "frac: \t\t" << std::hex << dut->frac_out << " \t| " << std::bitset<23>(dut->frac_out) << std::endl;
-
-    std::cout << "-----------------------------" << std::endl;
-
-    dut->clk = 0;
-    dut->in = 0b00111111100000000000000000000000; // 0
-    dut->eval();
-    dut->clk = !dut->clk;
-    dut->eval();
-    std::cout << "Converting: \t" << std::hex << dut->in << " \t| " << std::bitset<32>(dut->in) << std::endl;
-    std::cout << "int: \t\t" << std::hex << dut->int_out << " \t| " << std::bitset<2>(dut->int_out) << std::endl;
-    std::cout << "frac: \t\t" << std::hex << dut->frac_out << " \t| " << std::bitset<23>(dut->frac_out) << std::endl;
+    for (auto& i : kTestCases) {
+        dut->clk = 0; // falling edge
+        dut->in = i;
+        dut->eval();
+        dut->clk = 1; // rising edge
+        dut->eval();
+        std::cout << "-----------------------------" << std::endl;
+        std::cout << "Converting: \t" << std::bitset<32>(dut->in) << std::endl;
+        std::cout << "integer: \t" << std::bitset<2>(dut->int_out) << std::endl;
+        std::cout << "fractional: \t" << std::bitset<23>(dut->frac_out) << std::endl;
+    }
 
     // housekeeping
     tfp->close();
