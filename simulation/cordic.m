db9b9184ee29ace5ee26418ad6bb4f8be0ccfbea
Nik <nik.lewis21@imperial.ac.uk>
2024-03-07 13:45:25 +0000

    Cordic floating point functionality

--- simulation/cordic.m
+++ simulation/cordic.m
@@ -1,6 +1,56 @@
 % Simulation of CORDIC
+% Nik Lewis, CID 02031260
 
 close all;
+format long;
 
-% Conversion from floating point to fixed point.
+% Parameters to be tweaked to fit specification
+PARAM_WORD_LENGTH = 24; % 24 bit total wordlength extracted from FP->fixed conversion
+PARAM_FRACTION_LENGTH = 23; % 23 of these are fractional bits by default
+PARAM_N_ITERATIONS = 20;
+
+% Takes input as fixed-point number
+% theta = fi(0.45, true, PARAM_WORD_LENGTH, PARAM_FRACTION_LENGTH);
+
+iterations = 0:1:PARAM_N_ITERATIONS-1;
+alphas = calculate_alpha(iterations);
+
+% inits
+K = prod(sqrt(1 + 2.^(-2*(iterations))));
+x0 = fi(1/K, true, PARAM_WORD_LENGTH, PARAM_FRACTION_LENGTH);
+y0 = fi(0.0, true, PARAM_WORD_LENGTH, PARAM_FRACTION_LENGTH);
+z0 = fi(0.45, true, PARAM_WORD_LENGTH, PARAM_FRACTION_LENGTH);
+
+fprintf("Starting values: x0 = %f, y0 = %f, z0 = %f\n", x0, y0, z0);
+do_cordic(x0, y0, z0, PARAM_N_ITERATIONS, alphas);
+
+function [x, y, z] = do_cordic(x0, y0, z0, N, alphas)
+    % init
+    x = x0;
+    y = y0;
+    z = z0;
+    for iteration = 1:1:N
+        [x, y, z] = cordic_iteration(x, y, z, iteration, alphas);
+        fprintf("After iteration %d: x = %f, y = %f, z = %f\n", iteration, x, y, z);
+    end
+end
+
+% xi, yi, zi - current values
+% i - iteration number, starting from 0
+function [x, y, z] = cordic_iteration(xi, yi, zi, i, alphas)
+    % first op - lookup
+    alpha = alphas(i); % note matlab uses 1-based indexing
+    % display(alpha);
+    % get sign of current angle
+    di = sign(zi);
+    z = zi - di*alpha;
+
+    % update x and y
+    x = xi - di*yi*2^-(i-1);
+    y = yi + di*xi*2^-(i-1);
+end
+
+function alpha = calculate_alpha(i)
+    alpha = atand(2.^-i);
+end
 
